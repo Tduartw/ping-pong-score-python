0a3c9ee31a53288400fe1eaade99f4f660ebdf80
Taylan Duarte <taylan.duartee@gmail.com>
2021-11-04 23:57:46 -0300

    add score & toggle turn function & player winner logic

--- match.py
+++ match.py
@@ -1,30 +1,38 @@
 class Match:
 
     def __init__(self):
-        self.set = 1
+        self.sets = 0
         self.server_is = ''
 
-    def addPointTo(self, player):
+
+    def setTotalSets(self, interator):
+        self.sets = interator
+
+    def getTotalSets(self):
+        return self.sets
+        
+    def addScoreTo(self, player):
         new_score = 1
-        new_score += player.points 
+        new_score += player.getScore() 
         
         player.setScore(new_score)
-  
 
-    def addServeTo(self, player):
-        new_serve = 1
-        new_serve += player.serve 
+    def addGoldenScoreTo(self, player):
+        new_golden_score = 1
+        new_golden_score += player.getGoldenScore() 
         
-        player.setServe(new_serve)
+        player.setGoldenScore(new_golden_score)
 
 
-    def checkServeTurn(self, player_a, player_b):
-        self.server_is = player_a.name
-        
-        if player_a.getServe() == 5:
-            player_a.setServe(0)
-            self.server_is = player_b.name
+
+    def toggleTurn(self, player_a, player_b):
+        if player_a.getServe() == True:
+            player_b.setServe(True)
+            self.server_is = player_b.getName()
+            player_a.setServe(False)
         else:
-            player_b.setServe(0)
-            self.server_is = player_a.name
-       
+            player_a.setServe(True)
+            self.server_is = player_a.getName()
+            player_b.setServe(False)
+    
+ 
--- player.py
+++ player.py
@@ -1,15 +1,30 @@
 class Player:
     def __init__(self, name):
         self.name = name
-        self.serve = 0
-        self.points = 0
+        self.serve = False
+        self.score = 0
+        self.golden_score = 0
+    
+    
+    def getName(self):
+        return self.name
+
+    def setGoldenScore(self, golden_score):
+        self.golden_score = golden_score
+
+    def getGoldenScore(self):
+        return self.golden_score
+
+    def setScore(self, new_score):
+        self.score = new_score
+
+    def getScore(self):
+        return self.score
 
 
-    def setScore(self, new_points):
-        self.points = new_points
-
-    def setServe(self, new_serve):
-        self.serve = new_serve
+    def setServe(self, serve_state):
+        self.serve = serve_state
 
     def getServe(self):
         return self.serve
+
